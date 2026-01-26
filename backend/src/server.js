540ea7e37bcd3ef98d6283f7012a03f3d9b0dc68
Richwell111 <bcict20103@gmail.com>
2026-01-26 00:06:26 +0000

    created vite, added package json for the entre app

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,25 @@
 import express from 'express';
+import path from "path";
+import { ENV } from './config/env';
 
 const app = express();
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
-app.get('/api/health', (req, res) => {
-  res.status(200).json({ status: 'OK' ,message: 'Server is running smoothly'});
-  
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ message: "Success" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+  });
+}
+
+app.listen(ENV.PORT, () => {
+  console.log(`Server is running on http://localhost:${ENV.PORT}`);
 });
