269197315b3b3c05231f42cbb55799ea7295b68a
Richwell111 <bcict20103@gmail.com>
2026-01-26 05:20:02 +0000

    db config & tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,14 @@
-import express from 'express';
+import express from "express";
 import path from "path";
-import { ENV } from './config/env.js';
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
+app.use(express.json());
+app.use(clerkMiddleware()); // adds auth object under the req => req.auth
+// app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the browser to send the cookies to the server with the request
+
 const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
@@ -20,6 +26,11 @@ if (ENV.NODE_ENV === "production") {
   });
 }
 
-app.listen(ENV.PORT, () => {
-  console.log(`Server is running on http://localhost:${ENV.PORT}`);
-});
+const startServer = async () => {
+  await connectDB();
+  app.listen(ENV.PORT, () => {
+    console.log("Server is up and running");
+  });
+};
+
+startServer();
