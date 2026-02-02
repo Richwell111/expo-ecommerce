5bb46118c768b59ff4dd0b00c8a903bb4892d3c1
Richwell111 <bcict20103@gmail.com>
2026-02-02 08:16:55 +0000

    Admin routes and controllers added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,18 +4,27 @@ import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
-// import cors from "cors";
+import cors from "cors";
+import adminRoutes from "./routes/admin.route.js";
 
 import { functions, inngest } from "./config/inngest.js";
 const app = express();
 app.use(express.json());
 app.use(clerkMiddleware()); // adds auth object under the req => req.auth
-// app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the browser to send the cookies to the server with the request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the browser to send the cookies to the server with the request
 
 const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+
+
+app.use("/api/admin", adminRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/orders", orderRoutes);
+// app.use("/api/reviews", reviewRoutes);
+// app.use("/api/products", productRoutes);
+// app.use("/api/cart", cartRoutes);
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success" });
