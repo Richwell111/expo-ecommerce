b188b2b9d8444d65332fbef8e2b6e5352921f9b0
Richwell111 <bcict20103@gmail.com>
2026-02-02 12:11:54 +0000

    Made changes ihn the server, added cors package

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import path from "path";
-import { ENV } from "./config/env.js";
-import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
 import cors from "cors";
+
+import { functions, inngest } from "./config/inngest.js";
+
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+
 import adminRoutes from "./routes/admin.route.js";
 import userRoutes from "./routes/user.route.js";
 import orderRoutes from "./routes/order.route.js";
@@ -13,17 +17,29 @@ import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 
-import { functions, inngest } from "./config/inngest.js";
 const app = express();
+
+const __dirname = path.resolve();
+
+// special handling: Stripe webhook needs raw body BEFORE any body parsing middleware
+// apply raw body parser conditionally only to webhook endpoint
+app.use(
+  "/api/payment",
+  (req, res, next) => {
+    if (req.originalUrl === "/api/payment/webhook") {
+      express.raw({ type: "application/json" })(req, res, next);
+    } else {
+      express.json()(req, res, next); // parse json for non-webhook routes
+    }
+  },
+  paymentRoutes,
+);
+
 app.use(express.json());
 app.use(clerkMiddleware()); // adds auth object under the req => req.auth
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the browser to send the cookies to the server with the request
 
-const __dirname = path.resolve();
-const PORT = ENV.PORT || 3000;
-
 app.use("/api/inngest", serve({ client: inngest, functions }));
-
 
 app.use("/api/admin", adminRoutes);
 app.use("/api/users", userRoutes);
@@ -47,7 +63,7 @@ if (ENV.NODE_ENV === "production") {
 
 const startServer = async () => {
   await connectDB();
-  app.listen(PORT, () => {
+  app.listen(ENV.PORT, () => {
     console.log("Server is up and running");
   });
 };
