e7a5856973b0591b64681141a88823f2d7551779
Richwell111 <bcict20103@gmail.com>
2026-02-21 11:41:58 +0000

    init: Set up initial project structure and core configurations for an Expo React Native e-commerce application.

---
+++ mobileapp/app/(auth)/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text, Image, TouchableOpacity, ActivityIndicator } from "react-native";
+const AuthScreen = () => {
+    return (
+        <View>
+            <Text>AuthScreen</Text>
+        </View>
+    )
+}
+
+export default AuthScreen
